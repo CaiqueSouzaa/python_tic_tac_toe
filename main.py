5ef0bb90cf371b1abfaa8e768fb73511d60319a6
CaiqueSouzaa <caique.souzaa903@gmail.com>
2023-05-13 19:41:37 -0300

    commit2

--- main.py
+++ main.py
@@ -2,46 +2,57 @@ def getValue():
     try:
         coord_x = int(input('Type the coordinate X:'))
     except:
-        print('Invalid value for coordinate X. Please, try again.')
+        print("-------------------\nInvalid value for coordinate X. Please, try again or type '69' for exit of game.\n-------------------")
+        try:
+            if coord_x == 69:
+                game(0, 1)
+        except:
+            game()
         #print("This value isn't valid.")
-        exit()
 
     try:
         coord_y = int(input('Type the coordinate Y:'))
     except:
-        print('Invalid value for coordinate Y. Please, try again.')
-        exit()
+        print("Invalid value for coordinate Y. Please, try again or type '69' for exit of game.")
+        try:
+            if coord_y == 69:
+                game(0, 1)
+        except:
+            game()
 
-    return ((coord_x), (coord_y))
+    return ((coord_x - 1), (coord_y - 1))
 
 def updateTabuleiro(matriz):
-    tabuleiro = f'{matriz[0][0]} {matriz[0][1]} {matriz[0][2]}\n{matriz[1][0]} {matriz[1][1]} {matriz[1][2]}\n{matriz[2][0]} {matriz[2][1]} {matriz[2][2]}'
+    tabuleiro = f'X-1 {matriz[0][0]} {matriz[0][1]} {matriz[0][2]}\nX-2 {matriz[1][0]} {matriz[1][1]} {matriz[1][2]}\nX-3 {matriz[2][0]} {matriz[2][1]} {matriz[2][2]}'
     return tabuleiro
 
-def game(play = 0):
-    while play < 9:
-        if play % 2 == 0:
-            print('Player 1')
-            coord_x, coord_y = getValue()
-            if matriz[coord_x][coord_y] == '*':
-                matriz[coord_x][coord_y] = 'X'
-                tabuleiro = updateTabuleiro(matriz)
-                print(tabuleiro)
+def game(play = 0, stop = 0):
+    if stop == 0:
+        while play < 9:
+            if play % 2 == 0:
+                print('Player 1')
+                coord_x, coord_y = getValue()
+                if matriz[coord_x][coord_y] == '*':
+                    matriz[coord_x][coord_y] = 'X'
+                    tabuleiro = updateTabuleiro(matriz)
+                    print(tabuleiro)
+                else:
+                    print('This local is used. Please, choose a other local.')
+                    game(play)
             else:
-                print('This local is used. Please, choose a other local.')
-                game(play)
-        else:
-            print('Player 2')
-            coord_x, coord_y = getValue()
-            if matriz[coord_x][coord_y] == '*':
-                matriz[coord_x][coord_y] = 'O'
-                tabuleiro = updateTabuleiro(matriz)
-                print(tabuleiro)
-            else:
-                print('This local is used. Please, choose a other local')
-                game(play)
+                print('Player 2')
+                coord_x, coord_y = getValue()
+                if matriz[coord_x][coord_y] == '*':
+                    matriz[coord_x][coord_y] = 'O'
+                    tabuleiro = updateTabuleiro(matriz)
+                    print(tabuleiro)
+                else:
+                    print('This local is used. Please, choose a other local')
+                    game(play)
 
-        play += 1
+            play += 1
+    else:
+        return 1
 
 
 matriz = [
@@ -52,6 +63,5 @@ matriz = [
 
 tabuleiro = updateTabuleiro(matriz)
 
-print(f'-----\n{tabuleiro}\n-----')
+print(f'    -----\n{tabuleiro}\n    -----')
 game_play = game()
-
